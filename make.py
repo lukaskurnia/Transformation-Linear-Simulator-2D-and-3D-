a77af63f0e7486023b9e628050eb1945630b4ba5
willysantoso05 <41372609+willysantoso05@users.noreply.github.com>
2018-11-13 20:26:35 +0700

    update validasi input titik  2 dimensi

--- make.py
+++ make.py
@@ -1,14 +1,46 @@
 import numpy as np
 # Make
 
+def isNValid2D(x):
+    i = 0
+    try:
+        i = int(x)
+        if(i>=3):
+            integer=True
+        else:
+            integer=False
+    except:
+        integer = False
+    return integer
+
+def isTitikValid(x):
+    i = 0
+    val = False
+    while(i<len(x) and val==False):
+        if(x[i]==','):
+            val = True
+        else:
+            i = i+1
+    return val
+
 def twodimension():
     "Menu 2 Dimensi"
     print('Input banyaknya titik sudut bidang anda : ')
-    N = (int(input('>> ')))
+    x = input('>> ')
+    while (isNValid2D(x)==False):
+        print('Input harus bilangan bulat minimal 3')
+        x = input('>> ')
+    #input harus >= 3
+
+    N = (int(x))
     Mat = np.zeros( (N,2) )
     print('\nInput titik sudut bidang : ')
     for i in range(N):
+        print('Input titik ke ', i+1)
         S = input()
+        while (isTitikValid(S)==False):
+            print('Input titik ke ', i+1)
+            S = input()
         Ss = S.split(',')
         Mat[i][0] = (int(Ss[0]))
         Mat[i][1] = (int(Ss[1]))
